44a8f727d5a8ed3e047f0841b55aabc3fef92e46
farmerworking <farmerworking@gmail.com>
2024-08-01 09:12:13 +0800

    thymeleaf integration

---
+++ src/main/java/com/farmerworking/tiktok/viewer/web/Web.java
@@ -0,0 +1,21 @@
+package com.farmerworking.tiktok.viewer.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+
+@Controller
+public class Web {
+    @GetMapping("/example")
+    public String example(
+            @RequestParam(name = "username", required = false, defaultValue = "") String username,
+            Model model) {
+        model.addAttribute("message", "username is " + username);
+        model.addAttribute("tasks", Arrays.asList("a", "b", "c", "d", "e", "f", "g"));
+
+        return "example"; //view
+    }
+}
