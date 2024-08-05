e31cd18d6e81597939674db309d989cffe4563ca
farmerworking <farmerworking@gmail.com>
2024-08-05 08:29:32 +0800

    form and condition render for username

--- src/main/java/com/farmerworking/tiktok/viewer/web/controller/Web.java
+++ src/main/java/com/farmerworking/tiktok/viewer/web/controller/Web.java
@@ -1,14 +1,19 @@
 package com.farmerworking.tiktok.viewer.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
 
 import java.util.Arrays;
 
 @Controller
 public class Web {
+    private static Logger LOGGER = LoggerFactory.getLogger(Web.class);
+
     @GetMapping("/example")
     public String example(
             @RequestParam(name = "username", required = false, defaultValue = "") String username,
@@ -19,8 +24,14 @@ public class Web {
         return "example"; //view
     }
 
-    @GetMapping("/index")
-    public String index(Model model) {
+    @GetMapping("/")
+    public String index(
+            @RequestParam(name = "username", required = false, defaultValue = "") String username,
+            Model model) {
+        if (!StringUtils.isEmpty(username)) {
+            LOGGER.info("search for: " + username);
+            model.addAttribute("username", username);
+        }
         return "index";
     }
 }
